b9286ed04715568b94de8b002ffa6666819105a7
gabrirm <gabrirm00@gmail.com>
2023-08-24 13:02:20 +0200

    implemented cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // GLOBAL MIDDLEWARES
+// cors
+// simple requests
+app.use(cors());
+// non simple requests (delete, update...)
+app.options('*', cors());
 // Securing HTTP headers
 app.use(helmet());
 // Further HELMET configuration for Security Policy (CSP)
